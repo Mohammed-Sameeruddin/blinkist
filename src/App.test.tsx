514a28f0d4be82d55ddc4dfb5b566be647b71b68
Mohammed-Sameeruddin <msameeruddin2001@gmail.com>
2022-03-21 19:03:09 +0530

    coverage updated

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { screen } from "@testing-library/react";
-// import { Router } from "react-router";
-// import { createMemoryHistory } from "history";
-// import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-
-test("renders learn react link", () => {
-  screen.debug();
-});
